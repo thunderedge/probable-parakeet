76269af8e234aa6b5615cd25a2e382d185b3031d
v <39436798+thunderedge@users.noreply.github.com>
2019-05-18 17:13:39 +0200

    Update and rename HwDingetje.java to OP1+2.java

--- OP1+2.java
+++ OP1+2.java
@@ -6,20 +6,24 @@ import java.util.Scanner;
  *
  * @author adeliae
  */
+
 public class HwDingetje {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //opgave1();
-        opgave2();
+        opgave1();
+        //opgave2();
     }
     
     public static void opgave1(){
         //met maar één printline huh?
-        System.out.println("*\n**\n***\n****\n*****");
+        //System.out.println("*\n**\n***\n****\n*****"); //Щ(･｀ω´･Щ)
+        //jkjk
+        //looooooooooooooooooooooooooooooooooooooooooooop:
+        for(int i = 0; i < 5; i++){
+           for(int j = 0; j < 4; j++){
+               //print zooi uit.
+           } 
+        }
     }
     
     public static void opgave2(){
@@ -28,14 +32,17 @@ public class HwDingetje {
         
         double kelvin = 273.15; //celsius + < wat daar staat.
         int wiegebruiktditbehalveamerika = 0; //celsius * 1.8 + 32
+        //aah okay ik had de opdracht beter moeten lezen, convert van celsius naar fahrenheit/kevin!
         
         Scanner mcScanFace = new Scanner(System.in); //scanner aanroepen omdat we user input nodig hebben
         //nee fuck updates, stop windhoos
+        
         //uh ja het zou helpen als je ook iets in de console laat zien zodat de gebruiker ook weet wat ie moet doen.
         System.out.println("> enter zooi");
-        System.out.print("* ");
+        System.out.print("* "); //just to be extra fancy
         
         double input = mcScanFace.nextDouble(); //inputmismatchexception? really?? waar is de mismatch???
-        System.out.println(input); //je moet een komma gebruiken??? wat.
+        //nvm, kreeg het omdat ik geen komma had ingetoetst
+        //System.out.println(input); 
     }
 }
