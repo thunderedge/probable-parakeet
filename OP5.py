621b3e0c89259dfae2fab4fde1db84df0167bd95
v <39436798+thunderedge@users.noreply.github.com>
2019-05-24 03:03:07 +0200

    html/js >> py

---
+++ OP5.py
@@ -0,0 +1,18 @@
+from datetime import * #< ssshhhh ik that i should be more specific w/ my imports
+
+#laten we de docs erbij halen want ik heb geen idee waar ik moet beginnen
+#heh notes: Division by 0 raises ZeroDivisionError. mijn fav error/exception :D
+
+#dit is tenminste wat overzichterlijker dan de hele switch statement in de vorige versie
+#what was i even doing
+#ik heb een counter nodig voor elke x dat ik // typte voor een comment, jeez
+
+uur = datetime.now().hour #cast naar int want ik kreeg ruzie met de ide
+panda = input(">> Voer je naam in. \n * : ") #vraag om naam
+
+if uur >= 6 and uur <= 11:
+    print(">> Goedemorgen, " + panda + ".") #greet
+elif uur >= 12 and uur <= 17:
+    print(">> Goedemiddag, " + panda + ".")
+else:
+    print(">> Goedenavond, " + panda + ".")
