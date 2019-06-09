f6919cd67880164048ad1587704f8c85589da01e
v <39436798+thunderedge@users.noreply.github.com>
2019-06-09 22:11:05 +0200

    heewwwf

--- nietechtOP7/EventThingy.java
+++ nietechtOP7/EventThingy.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class EventThingy{
     private String eventName;
+    private double sRecord;
     private ArrayList<Student> students; //arraylist met studenten die meedoen
 
     public EventThingy(String eventName){
@@ -11,15 +12,48 @@ public class EventThingy{
         this.students = new ArrayList<>();
     }
 
-    public void setEventName(String eventName) {
+    public void setEventName(String eventName) { //setters
         this.eventName = eventName;
     }
 
-    public String getEventName(){
+    public String getEventName(){ //getters
         return this.eventName;
     }
 
     public void addStudent(Student student){
-        this.students.add(student); //en voeg ze teoe
+        this.students.add(student); //voegt studenten toe aan arraylist
+    }
+
+    public void addStudentRecord(String sName, double records){
+        this.sRecord = records;
+
+        for(int i = 0; i < this.students.size(); i++){ //whee for loop
+            if(this.students.get(i).getName().equals(sName)){ //lopot foor list n kwjkt of naam matchet me t studnet naam.
+                this.students.get(i).setStudentRecord(records); //dna udpate ie de record
+            }
+        }
+    }
+
+    public Student bestRecord() { //boildean, sushi really wou je osms ene nieweu daete type verzinnen?
+
+        Student bestRecord = this.students.get(0); //referentiedingpunt
+
+        for(Student s: this.students){
+            if(s.getStudentRecord() > bestRecord.getStudentRecord()){ //kijkt f record hoger/btr is dna ref en cpoypsta dat erin ald dat do is
+                bestRecord = s;
+            }
+        }
+
+        return bestRecord;
+    }
+
+    public String toString(){
+        String sName = ""; //init
+
+        for(Student s: this.students){
+            sName += " " + s.getName() + " Record: " + s.getStudentRecord() + "\n"; //loopt door lijst, haalt alle studenten op
+        }
+
+        return sName;
     }
 }
